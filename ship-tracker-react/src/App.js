10e7b781254f9e0bbf6b9454972e13ec41e1edec
Tom Langton <tom@pulpinfo.com>
2017-06-29 08:39:50 -0400

    thurs morn

--- ship-tracker-react/src/App.js
+++ ship-tracker-react/src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    axios.get("/companies").then(res => {
+      this.setState({
+        companies: res.data
+      });
+    });
+  }
+
+  renderCompany(company) {
+    return (
+      <h3 key={company.id}>
+        {company.title}
+      </h3>
+    );
+  }
+
+  renderCompanies() {
+    if (this.state.companies) {
+      return (
+        <div className="companies">
+          {this.state.companies.map(this.renderCompany)}
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>hi</h1>
+        {this.renderCompanies()}
       </div>
     );
   }
